a88ba8777c565ebf36dddc4ed16b06ed32c819f5
Yago Pajariño <yagopajarino@outlook.com>
2025-01-18 18:56:13 -0300

    Prettier publish.py

--- publish.py
+++ publish.py
@@ -183,22 +183,23 @@ RSS_MAIN_TEMPLATE = """
 </rss>
 """
 
+
 def extract_metadata(fil, filename=None):
     metadata = {}
     if filename:
-        assert filename[-3:] == '.md'
-        metadata["filename"] = filename[:-3]+'.html'
+        assert filename[-3:] == ".md"
+        metadata["filename"] = filename[:-3] + ".html"
     while 1:
         line = fil.readline()
-        if line and line[0] == '[' and ']' in line:
-            key = line[1:line.find(']')]
-            value_start = line.find('(')+1
-            value_end = line.rfind(')')
-            if key in ('category', 'categories'):
-                metadata['categories'] = set([
-                    x.strip().lower() for x in line[value_start:value_end].split(',')
-                ])
-                assert '' not in metadata['categories']
+        if line and line[0] == "[" and "]" in line:
+            key = line[1 : line.find("]")]
+            value_start = line.find("(") + 1
+            value_end = line.rfind(")")
+            if key in ("category", "categories"):
+                metadata["categories"] = set(
+                    [x.strip().lower() for x in line[value_start:value_end].split(",")]
+                )
+                assert "" not in metadata["categories"]
             else:
                 metadata[key] = line[value_start:value_end]
         else:
@@ -208,98 +209,117 @@ def extract_metadata(fil, filename=None):
 
 def metadata_to_path(global_config, metadata):
     return os.path.join(
-        global_config.get('posts_directory', 'posts'),
-        metadata['date'],
-        metadata['filename']
+        global_config.get("posts_directory", "posts"),
+        metadata["date"],
+        metadata["filename"],
     )
 
 
 def generate_feed(global_config, metadatas):
     def get_link(route):
-        return global_config['domain'] + "/" + route
+        return global_config["domain"] + "/" + route
 
     def get_date(date_text):
-        year, month, day = (int(x) for x in date_text.split('/'))
+        year, month, day = (int(x) for x in date_text.split("/"))
         date = datetime.date(year, month, day)
-        return date.strftime('%a, %d %b %Y 00:00:00 +0000')
+        return date.strftime("%a, %d %b %Y 00:00:00 +0000")
 
     def get_item(metadata):
         return RSS_ITEM_TEMPLATE.format(
-            title=metadata['title'],
-            link=get_link('/'.join([global_config['posts_directory'], metadata['date'], metadata['filename']])),
-            pub_date=get_date(metadata['date']), description=''
+            title=metadata["title"],
+            link=get_link(
+                "/".join(
+                    [
+                        global_config["posts_directory"],
+                        metadata["date"],
+                        metadata["filename"],
+                    ]
+                )
+            ),
+            pub_date=get_date(metadata["date"]),
+            description="",
         )
 
     return RSS_MAIN_TEMPLATE.strip().format(
-        title=global_config['title'],
-        link=get_link(''),
-        icon=global_config['icon'],
-        items="\n".join(map(get_item, metadatas))
+        title=global_config["title"],
+        link=get_link(""),
+        icon=global_config["icon"],
+        items="\n".join(map(get_item, metadatas)),
     )
 
 
-
-
 def make_twitter_card(title, global_config):
-    return TWITTER_CARD_TEMPLATE.format(title, global_config['icon'])
+    return TWITTER_CARD_TEMPLATE.format(title, global_config["icon"])
 
 
 def defancify(text):
-    return text \
-        .replace("’", "'") \
-        .replace('“', '"') \
-        .replace('”', '"') \
-        .replace('…', '...') \
+    return (
+        text.replace("’", "'").replace("“", '"').replace("”", '"').replace("…", "...")
+    )
 
 
 def make_categories_header(categories, root_path):
-    o = ['<center><hr>']
+    o = ["<center><hr>"]
     for category in categories:
         template = '<span class="toc-category" style="font-size:{}%"><a href="{}/categories/{}.html">{}</a></span>'
-        o.append(template.format(min(100, 900 // len(category)), root_path, category, category.capitalize()))
-    o.append('<hr></center>')
-    return '\n'.join(o)
+        o.append(
+            template.format(
+                min(100, 900 // len(category)),
+                root_path,
+                category,
+                category.capitalize(),
+            )
+        )
+    o.append("<hr></center>")
+    return "\n".join(o)
 
 
 def get_printed_date(metadata):
-    year, month, day = metadata['date'].split('/')
-    month = 'JanFebMarAprMayJunJulAugSepOctNovDec'[int(month)*3-3:][:3]
-    return year + ' ' + month + ' ' + day
+    year, month, day = metadata["date"].split("/")
+    month = "JanFebMarAprMayJunJulAugSepOctNovDec"[int(month) * 3 - 3 :][:3]
+    return year + " " + month + " " + day
+
 
 def make_toc_item(global_config, metadata, root_path):
     link = metadata_to_path(global_config, metadata)
-    return TOC_ITEM_TEMPLATE.format(get_printed_date(metadata), root_path + '/' + link, metadata['title'])
+    return TOC_ITEM_TEMPLATE.format(
+        get_printed_date(metadata), root_path + "/" + link, metadata["title"]
+    )
 
 
 def make_toc(toc_items, global_config, all_categories, category=None):
     if category:
         title = category.capitalize()
-        root_path = '..'
+        root_path = ".."
     else:
-        title = global_config['title']
-        root_path = '.'
+        title = global_config["title"]
+        root_path = "."
 
     return (
-        PRE_HEADER +
-        RSS_LINK.format(root_path, title) +
-        HEADER_TEMPLATE.replace('$root', root_path) +
-        TOGGLE_COLOR_SCHEME_JS +
-        make_twitter_card(title, global_config) +
-        TOC_TITLE_TEMPLATE.format(title) +
-        make_categories_header(all_categories, root_path) +
-        TOC_START +
-        ''.join(toc_items) +
-        TOC_END
+        PRE_HEADER
+        + RSS_LINK.format(root_path, title)
+        + HEADER_TEMPLATE.replace("$root", root_path)
+        + TOGGLE_COLOR_SCHEME_JS
+        + make_twitter_card(title, global_config)
+        + TOC_TITLE_TEMPLATE.format(title)
+        + make_categories_header(all_categories, root_path)
+        + TOC_START
+        + "".join(toc_items)
+        + TOC_END
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Get blog config
-    global_config = extract_metadata(open('config.md'))
+    global_config = extract_metadata(open("config.md"))
 
     # Special case: '--sync' option
-    if '--sync' in sys.argv:
-        os.system('rsync -av site/. {}:{}'.format(global_config['server'], global_config['website_root']))
+    if "--sync" in sys.argv:
+        os.system(
+            "rsync -av site/. {}:{}".format(
+                global_config["server"], global_config["website_root"]
+            )
+        )
         sys.exit()
 
     # Normal case: process each provided file
@@ -313,67 +333,78 @@ if __name__ == '__main__':
         path = metadata_to_path(global_config, metadata)
 
         # Generate the html file
-        options = metadata.get('pandoc', '')
+        options = metadata.get("pandoc", "")
 
-        os.system('pandoc -o /tmp/temp_output.html {} {}'.format(file_location, options))
-        root_path = '../../../..'
+        os.system(
+            "pandoc -o /tmp/temp_output.html {} {}".format(file_location, options)
+        )
+        root_path = "../../../.."
         total_file_contents = (
-            PRE_HEADER +
-            RSS_LINK.format(root_path, metadata['title']) +
-            HEADER_TEMPLATE.replace('$root', root_path) +
-            TOGGLE_COLOR_SCHEME_JS +
-            make_twitter_card(metadata['title'], global_config) +
-            TITLE_TEMPLATE.format(metadata['title'], get_printed_date(metadata), root_path) +
-            defancify(open('/tmp/temp_output.html').read()) +
-            FOOTER
+            PRE_HEADER
+            + RSS_LINK.format(root_path, metadata["title"])
+            + HEADER_TEMPLATE.replace("$root", root_path)
+            + TOGGLE_COLOR_SCHEME_JS
+            + make_twitter_card(metadata["title"], global_config)
+            + TITLE_TEMPLATE.format(
+                metadata["title"], get_printed_date(metadata), root_path
+            )
+            + defancify(open("/tmp/temp_output.html").read())
+            + FOOTER
         )
 
         print("Path selected: {}".format(path))
 
         # Make sure target directory exists
         truncated_path = os.path.split(path)[0]
-        os.system('mkdir -p {}'.format(os.path.join('site', truncated_path)))
+        os.system("mkdir -p {}".format(os.path.join("site", truncated_path)))
 
         # Put it in the desired location
-        out_location = os.path.join('site', path)
-        open(out_location, 'w').write(total_file_contents)
+        out_location = os.path.join("site", path)
+        open(out_location, "w").write(total_file_contents)
 
     # Reset ToC
     metadatas = []
     categories = set()
-    for filename in os.listdir('posts'):
-        if filename[-4:-1] != '.sw':
-            metadatas.append(extract_metadata(open(os.path.join('posts', filename)), filename))
-            categories = categories.union(metadatas[-1]['categories'])
+    for filename in os.listdir("posts"):
+        if filename[-4:-1] != ".sw":
+            metadatas.append(
+                extract_metadata(open(os.path.join("posts", filename)), filename)
+            )
+            categories = categories.union(metadatas[-1]["categories"])
     categories = sorted(categories)
 
-    print("Detected categories: {}".format(' '.join(categories)))
+    print("Detected categories: {}".format(" ".join(categories)))
 
-    sorted_metadatas = sorted(metadatas, key=lambda x: x['date'], reverse=True)
+    sorted_metadatas = sorted(metadatas, key=lambda x: x["date"], reverse=True)
     feed = generate_feed(global_config, sorted_metadatas)
 
-    os.system('mkdir -p {}'.format(os.path.join('site', 'categories')))
+    os.system("mkdir -p {}".format(os.path.join("site", "categories")))
 
     print("Building tables of contents...")
 
     homepage_toc_items = [
-        make_toc_item(global_config, metadata, '.') for metadata in sorted_metadatas if
-        global_config.get('homepage_category', '') in metadata['categories'].union({''})
+        make_toc_item(global_config, metadata, ".")
+        for metadata in sorted_metadatas
+        if global_config.get("homepage_category", "")
+        in metadata["categories"].union({""})
     ]
 
     for category in categories:
         category_toc_items = [
-            make_toc_item(global_config, metadata, '..') for metadata in sorted_metadatas if
-            category in metadata['categories']
+            make_toc_item(global_config, metadata, "..")
+            for metadata in sorted_metadatas
+            if category in metadata["categories"]
         ]
         toc = make_toc(category_toc_items, global_config, categories, category)
-        open(os.path.join('site', 'categories', category+'.html'), 'w').write(toc)
+        open(os.path.join("site", "categories", category + ".html"), "w").write(toc)
 
-    open('site/feed.xml', 'w').write(feed)
-    open('site/index.html', 'w').write(make_toc(homepage_toc_items, global_config, categories))
+    open("site/feed.xml", "w").write(feed)
+    open("site/index.html", "w").write(
+        make_toc(homepage_toc_items, global_config, categories)
+    )
 
     # Copy CSS and scripts files
     this_file_directory = os.path.dirname(__file__)
-    os.system('cp -r {} site/'.format(os.path.join(this_file_directory, 'css')))
-    os.system('cp -r {} site/'.format(os.path.join(this_file_directory, 'scripts')))
-    os.system('rsync -av images site/')
+    os.system("cp -r {} site/".format(os.path.join(this_file_directory, "css")))
+    os.system("cp -r {} site/".format(os.path.join(this_file_directory, "scripts")))
+    os.system("rsync -av images site/")
