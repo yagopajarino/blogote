5a4fb1f5320079b315abe58cf3dbb60a0aaf2682
Yago Pajariño <pajarinoyago@gmail.com>
2026-03-21 20:27:44 -0300

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yagopajarino.github.io',
-	base: '/blogote/',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		remarkPlugins: [
