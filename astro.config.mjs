d6c28fcbd85df733c0756533bb60c6337ff6e16a
Yago Pajariño <pajarinoyago@gmail.com>
2026-03-26 20:07:20 -0300

    fix: update twitter card url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yagopajarino.github.io',
+	site: 'https://blog.yagopajarino.ar',
 	base: '/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
